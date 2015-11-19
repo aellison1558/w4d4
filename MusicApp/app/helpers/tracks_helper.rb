db664104907d96cf803d1c045f2bff04bcd82043
Andrew Ellison <aellison1558@gmail.com>
2015-11-19 15:44:58 -0800

    add ugly lyrics helper

--- MusicApp/app/helpers/tracks_helper.rb
+++ MusicApp/app/helpers/tracks_helper.rb
@@ -1,2 +1,12 @@
+include ERB::Util
 module TracksHelper
+
+  def ugly_lyrics(lyrics)
+    lyrics = lyrics.split("\n")
+    html = ""
+    lyrics.each do |lyric|
+      html += "<pre>#{h('♫ ' + lyric)}</pre>"
+    end
+    html.html_safe
+  end
 end
