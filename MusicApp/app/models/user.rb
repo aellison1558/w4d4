edfd32ca22d91b63db41a6f5618b6f1487710f0e
Andrew Ellison <aellison1558@gmail.com>
2015-11-19 11:07:18 -0800

    Add Users

---
+++ MusicApp/app/models/user.rb
@@ -0,0 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  validates
+    :email,
+    :password_digest,
+    :session_token,
+    presence: true,
+    uniqueness: true
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.base64
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    token = self.session_token = User.generate_session_token
+    self.save
+    token
+  end
+
+  def ensure_session_token!
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(given_password)
+    my_password = BCrypt::Password.new(self.password_digest)
+    my_password.is_password?(given_password)
+  end
+
+
+end
