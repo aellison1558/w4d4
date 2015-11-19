634e14a032978c29d7bc5c42237594a89b7fe14c
Andrew Ellison <aellison1558@gmail.com>
2015-11-19 11:27:01 -0800

    Add User Controller

--- MusicApp/app/models/user.rb
+++ MusicApp/app/models/user.rb
@@ -11,13 +11,10 @@
 #
 
 class User < ActiveRecord::Base
-  validates
-    :email,
-    :password_digest,
-    :session_token,
-    presence: true,
-    uniqueness: true
-  after_initialize :ensure_session_token
+  validates :email, :password_digest, :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token!
+  attr_reader :password
 
   def self.generate_session_token
     SecureRandom.base64
