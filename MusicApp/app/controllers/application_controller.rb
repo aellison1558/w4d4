8213bc5561bc0c4fa2e9ff5adc4e31ae4d40cfd1
Andrew Ellison <aellison1558@gmail.com>
2015-11-19 15:07:45 -0800

    require login to see stuff

--- MusicApp/app/controllers/application_controller.rb
+++ MusicApp/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :require_login, except: [:index]
+
   helper_method :current_user, :logged_in?
 
   def current_user
@@ -20,5 +22,9 @@ class ApplicationController < ActionController::Base
   def logout!
     current_user.reset_session_token!
     session[:session_token] = nil
+  end
+
+  def require_login
+    redirect_to root_url unless logged_in?
   end
 end
