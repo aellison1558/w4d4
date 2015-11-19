514d4f5d585c4370fee88f1b3707395dd38a8693
Andrew Ellison <aellison1558@gmail.com>
2015-11-19 13:56:36 -0800

    Band controller and views

--- MusicApp/config/routes.rb
+++ MusicApp/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  root to: 'bands#index'
+
   resources :users
   resource :session, only: [:new, :create, :destroy]
+
+  resources :bands do
+    resources :albums, only: [:index]
+  end
+
+  resources :albums, only: [:show, :new, :create, :destroy] do
+    resources :tracks, only: [:index]
+  end
+
+  resources :tracks, only: [:show, :new, :create, :destroy]
 end
