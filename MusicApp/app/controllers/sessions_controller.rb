14e9024572677952e8464adf2222cff675d2cdf6
Andrew Ellison <aellison1558@gmail.com>
2015-11-19 11:37:04 -0800

    Login/Logout

--- MusicApp/app/controllers/sessions_controller.rb
+++ MusicApp/app/controllers/sessions_controller.rb
@@ -2,9 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create 
+  def create
+    @user = User.find_by_credentials(email, password)
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
   end
 
   def destroy
+    logout
+    redirect_to new_session_url
+  end
+
+  private
+  def email
+    params[:login][:email]
+  end
+
+  def password
+    params[:login][:password]
   end
 end
--- MusicApp/app/controllers/users_controller.rb
+++ MusicApp/app/controllers/users_controller.rb
@@ -7,10 +7,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
 
-      redirect_to '/'
+      redirect_to user_url(@user)
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
